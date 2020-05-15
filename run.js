7816cb336b387aa345c14a14e898ab4764faee63
dhowe <dhowe@cims.nyu.edu>
2020-05-15 21:10:14 +0800

    add ex for location

--- run.js
+++ run.js
@@ -13,5 +13,23 @@ let rt = new RiTwit(conf);
 0 && rt.userById('1241263348529807361');
 
 0 && rt.onTweetMatching({ follow: '1241263348529807361' }, function (tweet) {
-  console.log(tweet);
 })
+
+0 && rt.onTweetMatching({ track: 'COVID', language: 'en', locations: '-74,40,-73,41' }, function (tweet) {
+   if (tweet.retweeted_status) return; // no retweets
+   if (!/#COVID/.test(tweet.text)) return; // must contain #COVID hashtag
+   let text = cleanTweet(tweet.text);
+   console.log(text, '\n---------------------------------------');
+})
+
+function cleanTweet(tweet) {
+  let result = [], words = RiTa.tokenize(tweet);
+  for (let i = 0; i < words.length; i++) {
+    if (words[i] === '@') { i++; continue; }
+    if (words[i].match(/^[';.?!;,"]$/)) continue;
+    if (!words[i].match(/^[A-z]+$/)) return false;
+    result.push(words[i]);
+  }
+  return RiTa.untokenize(result);
+}
+
