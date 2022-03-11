614476e92bab38d3a3b6d05d91a692b60a59887a
dhowe <dhowe@cims.nyu.edu>
2022-03-11 10:48:11 +0800

    update config

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  consumer_key: 'fHmL1qY0v9wtu5BMvOHEzq4Uf',
-  consumer_secret: '8OI151mM4oE30kHMkGIZVkHu7c8nDsH01OdSGJSIUcpxOG8PZF',
-  access_token: '1241263348529807361-TeZxiiyAl3hK5qjw2MXTWzzkNnsOhO',
-  access_token_secret: '6PjbsNKGq0XASzjp76yzEEXaEVs7VWlUTTKnaEBXlW07o',
+  consumer_key: 'dummyfHmL1qY0tu5BMvOHEzq4Uf',
+  consumer_secret: 'dummyI151mM4okHMkGIZVkHu7c8nOdSGJSIUcpxOG8PZF',
+  access_token: 'dummy1241263348529801-TeZxiiyAl3hK5qjw2MXTWzzkNnsOhO',
+  access_token_secret: 'dummy6PjbsNKGASzjp76yzEEXaEVs7VWlUTTKnaEBXlW07o',
   verbose: true
 }
